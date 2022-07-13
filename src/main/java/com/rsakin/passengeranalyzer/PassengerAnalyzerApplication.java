17d11e93198dfec5a8a643f7dacf88c99e3586b2
rsakin <ramazan.sakin@solve.care>
2022-07-13 16:38:29 +0300

    project init

---
+++ src/main/java/com/rsakin/passengeranalyzer/PassengerAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.rsakin.passengeranalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rsakin/passengeranalyzer/PassengerAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsakin.passengeranalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
